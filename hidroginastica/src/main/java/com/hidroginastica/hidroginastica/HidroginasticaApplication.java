8652ea5bbbb5473c1d62d0fc8540de77e4f125d2
analu <analu@10.104.2.214>
2019-11-27 10:15:41 -0200

    Clonando Projeto.

---
+++ hidroginastica/src/main/java/com/hidroginastica/hidroginastica/HidroginasticaApplication.java
@@ -0,0 +1,13 @@
+package com.hidroginastica.hidroginastica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HidroginasticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HidroginasticaApplication.class, args);
+	}
+
+}
